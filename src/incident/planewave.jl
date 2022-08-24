ee8c9cebdee87fe8aeaf028b3bc1d2b1f2e64fe4
Matthew Priddin <mjp98@cam.ac.uk>
2022-08-24 22:07:28 +0100

    Force wave parameters to be complex

--- src/incident/planewave.jl
+++ src/incident/planewave.jl
@@ -16,6 +16,7 @@ struct PlaneWave{N,T} <: Function
     ω::T
     A::T
     function PlaneWave(k::SVector{N,S}, ω=0, A=1) where {N,S}
+        ω = complec(ω)
         T = eltype(promote(k[1], ω, A))
         new{N,T}(convert.(T, k), convert(T, ω), convert(T, A))
     end
